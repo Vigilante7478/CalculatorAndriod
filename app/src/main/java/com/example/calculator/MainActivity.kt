34cb4367cebf06dc6fee05fbe1396ebd78399334
Vigilante7478 <vanditthakkar09@gmail.com>
2019-12-20 15:45:18 +0530

    Commit on Divide

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -17,5 +17,13 @@ class MainActivity : AppCompatActivity() {
             val n3: Int = n1 + n2
             ans.setText(n3.toString())
         }
+
+        div.setOnClickListener(){
+            val n1: Float = no1.getText().toString().toFloat()
+            val n2: Float = no2.getText().toString().toFloat()
+
+            val n3: Float = n1 / n2
+            ans.setText(n3.toString())
+        }
     }
 }
