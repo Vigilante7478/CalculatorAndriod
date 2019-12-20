69c7ca567fbdd59bf0ad4abfba3901ee25241df2
Vigilante7478 <vanditthakkar09@gmail.com>
2019-12-20 15:33:04 +0530

    Commit on master branch

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sum.setOnClickListener(){
+            val n1: Int = no1.getText().toString().toInt()
+            val n2: Int = no2.getText().toString().toInt()
+
+            val n3: Int = n1 + n2
+            ans.setText(n3.toString())
+        }
     }
 }
